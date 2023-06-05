8bee7860c400c417e88f6ca003a55ce2a27da588
shayshahal <shay840@gmail.com>
2023-06-05 08:39:10 +0300

    add lead creation POST route

--- app.py
+++ app.py
@@ -14,5 +14,16 @@ FacebookAdsApi.init(my_app_id, my_app_secret, access_token)
 load_dotenv()
 
 
+@app.route('/', methods=['POST'])
+def hello():
+    if ('name' in request.args):
+        page = Page(os.getenv('PAGE_ID'))
+        page.create_lead_gen_form([], {'name': request.args.get(
+            'name'), 'questions': [{'type': 'EMAIL'}, {'type': 'FULL_NAME'}], 'privacy_policy': {'url': 'www.google.com'}, 'follow_up_action_url': 'www.google.com'})
+        return 'success', 200
+    else:
+        return 'bad args, must have name', 400
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
