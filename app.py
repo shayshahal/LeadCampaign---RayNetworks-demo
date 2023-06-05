c48f37f1d494722ef9e6c64b4612ad374cd6b807
shayshahal <shay840@gmail.com>
2023-06-05 15:32:35 +0300

    add GET route

--- app.py
+++ app.py
@@ -20,6 +20,27 @@ def createLead():
                                         'type': 'FULL_NAME'}], 'privacy_policy': {'url': 'www.google.com'}, 'follow_up_action_url': 'www.google.com'})
     return 'success', 200
 
+@app.route('/leads-campaign')
+def getLeadsFromLast24H():
+    page = Page(page_id)
+    leadGens = page.get_lead_gen_forms(['id'], {'name': 'leads-campaign'})
+    fields = ['id', 'created_time', 'field_data']
+    params = {
+        'filtering': [{'field': 'time_created', 'operator': 'GREATER_THAN', 'value': time.time()-86400}],
+    }
+    leads = LeadgenForm(leadGens[0]['id']).get_leads(
+        fields=fields, params=params)
+
+    for res in leads:
+        fieldDataString = ','.join('{0}: {1}'.format(
+            obj['name'], obj['values']) for obj in res['field_data'])
+        lead = Lead(
+            id=res['id'],
+            created_time=res['created_time'],
+            fieldData=fieldDataString
+        )
+        db.session.add(lead)
+    db.session.commit()
     return 'success', 200
 
 
